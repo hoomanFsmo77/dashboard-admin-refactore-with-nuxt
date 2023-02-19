3bcde526621f0f351f2f726c1e4789da8461a28e
hooman mousavi <hoomanmousavi77@gmail.com>
2023-02-19 22:04:01 +0330

    dashoards >>> charts and projects completed

---
+++ src/components/card/Project.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="card p-1.3">
+    <div class="flex justify-between items-start">
+      <div>
+        <span class="font-700 text-[0.9rem] text-primary-dark">{{title}}</span>
+        <h3 class="font-700" :class="$textColor(theme)">{{achieve}}</h3>
+      </div>
+      <span class="icon-wrapper !h-3 !w-3" :class="$bgColor(theme)">
+                    <i class="text-1.5" :class="[icon,$textColor(theme)]"></i>
+                  </span>
+    </div>
+    <div>
+      <span class="text-gray-700 text-0.75">May 23 - June 01 (2018)</span>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const {$textColor,$bgColor}=useNuxtApp()
+let props=defineProps(['title', 'achieve', 'theme','icon'])
+
+
+</script>
+
--- src/plugins/fontawesome.ts
+++ src/plugins/fontawesome.ts
@@ -1,10 +1,10 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import {  faAlignJustify,faEnvelope,faArrowUpFromBracket} from '@fortawesome/free-solid-svg-icons'
-import {faTwitter} from '@fortawesome/free-brands-svg-icons'
+import {  faAlignJustify,faEnvelope,faArrowUpFromBracket,faStore,faBatteryThreeQuarters,faDollarSign,faUserGroup} from '@fortawesome/free-solid-svg-icons'
+import {faTwitter,faApple,faDropbox,faAndroid} from '@fortawesome/free-brands-svg-icons'
 import {faNoteSticky} from '@fortawesome/free-regular-svg-icons'
 
 export default defineNuxtPlugin(nuxtApp=>{
-    library.add(faAlignJustify,faTwitter,faEnvelope,faArrowUpFromBracket,faNoteSticky)
+    library.add(faAlignJustify,faTwitter,faEnvelope,faArrowUpFromBracket,faNoteSticky,faApple,faDropbox,faAndroid,faStore,faBatteryThreeQuarters,faDollarSign,faUserGroup)
     nuxtApp.vueApp.component('font-awesome-icon', FontAwesomeIcon)
 })
