311f2bfc3749e0c4345e6a6a205bb3d131cf951a
hooman mousavi <hoomanmousavi77@gmail.com>
2023-02-26 11:20:57 +0330

    pages >>> contacts & invoice & profile & pricing table completed

---
+++ src/components/card/Contact.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="card p-1.5 flex gap-1 relative items-center !rounded-[20px]">
+      <div>
+        <UserProfile class="overflow-hidden" height="80px" width="80px" :src="image" img-class="object-cover rounded-full h-[88%]" image-width="70"/>
+      </div>
+    <div>
+      <span class="block text-1.1 text-primary-dark font-800">{{name}}</span>
+      <span class="text-[0.94rem] tracking-widest font-400 uppercase text-gray-700 block mb-0.85">{{job}}</span>
+      <span class="text-[0.9rem] text-gray-800 break-all ">{{email}}</span>
+      <br>
+      <span class="text-[0.9rem] text-gray-800 break-all"> <abbr title="phone">P:</abbr>{{phone}}</span>
+    </div>
+    <NuxtLink class="link-stretch" :to="{name:'Pages-Profile'}"></NuxtLink>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "ContactBox",
+  props:['job','image','name','email','phone']
+}
+</script>
+
--- src/pages/Pages/Contacts.vue
+++ src/pages/Pages/Contacts.vue
@@ -1,13 +1,23 @@
 <template>
-  Contacts
+  <div class="container-fluid">
+    <row>
+      <column>
+        <Breadcrumb :pages="[{name:'home',link:'index'},{name:'Contacts'}]"/>
+        <h1 class="text-primary-dark uppercase tracking-widest !px-0 mt-1.2">Contacts</h1>
+      </column>
+    </row>
+    <row>
+      <column v-for="(item,index) in contactData" :key="index" col="12" md="4">
+        <CardContact :job="item.job" :image="item.image" :name="item.name" :email="item.email" :phone="item.phone"/>
+      </column>
+    </row>
+  </div>
+
+
+
 </template>
 
-<script>
-export default {
-  name: "Contacts"
-}
+<script setup lang="ts">
+import {contactData} from "~/utils/stateData";
 </script>
 
-<style scoped>
-
-</style>
